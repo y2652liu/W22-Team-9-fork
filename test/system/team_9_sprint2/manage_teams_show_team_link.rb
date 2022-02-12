324a1afbca7d46c6aaac53ae021e18754d5e46da
Ahmed Ismail <awismail@uwaterloo.ca>
2022-02-12 18:26:40 -0500

    Hyperlink show to name, removing direct show link: Manage Teams Edits

---
+++ test/system/team_9_sprint2/manage_teams_show_team_link.rb
@@ -0,0 +1,34 @@
+require "application_system_test_case"
+
+class ShowTeamLink < ApplicationSystemTestCase
+# I modeled this test class off of DisplayFeedbackHistoryUser.rb (mostly) created in Sprint 1
+
+    setup do
+        # create prof, team, and user
+        @prof = User.create(email: 'msmucker@gmail.com', name: 'Mark Smucker', is_admin: true, password: 'professor', password_confirmation: 'professor')
+        
+        @team = Team.create(team_name: 'Test Team', team_code: 'TEAM01', user: @prof)
+        
+        @mike = User.create(email: 'bob@gmail.com', name: 'Mike', is_admin: false, password: 'testpassword', password_confirmation: 'testpassword')
+        @mike.teams << @team
+        
+    end
+
+    def test_team_name_link_show
+        visit root_url 
+        login 'msmucker@gmail.com', 'professor'
+        click_on "Manage Teams"
+        assert_current_path teams_url
+ 
+      within('#team' + @team.id.to_s) do 
+        click_on 'Test Team'
+      end
+      assert_current_path team_url(@team)
+
+      end
+      
+  end
+
+
+
+    
